4fe6ee9d646c4686dcba53470b6417b85588ae24
Nhan Tri Danh <57444770+dnhan1707@users.noreply.github.com>
2024-11-26 09:24:06 -0800

    Installed necessary libraries from reproduce issue code

--- App.tsx
+++ App.tsx
@@ -1,118 +1,61 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { View, Text } from 'react-native';
+import { Button, LinearProgress } from '@rneui/themed';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const LinearProgressAPI: React.FunctionComponent = () => {
+const [progress, setProgress] = React.useState(0);
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+React.useEffect(() => {
+  let subs = true;
+  if (progress < 1 && progress !== 0) {
+    setTimeout(() => {
+      if (subs) {
+        setProgress(progress + 0.1);
+      }
+    }, 100);
+  }
+  return () => {
+    subs = false;
   };
+}, [progress]);
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+return (
+  <View>
+    <View
+      style={{
+        margin: 10,
+      }}
+    >
+      <Text>Indeterminate Variant </Text>
+      <LinearProgress style={{ marginVertical: 10 }} />
+      <Text>Indeterminate Variant with color</Text>
+      <LinearProgress style={{ marginVertical: 10 }} color="red" />
+      <Text>Determinate Variant</Text>
+      <LinearProgress
+        style={{ marginVertical: 10 }}
+        value={progress}
+        variant="determinate"
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+      <Button
+        disabled={progress > 0}
+        onPress={() => {
+          setProgress(0.00001);
+        }}
+        title={'Start Progress'}
+        containerStyle={{ margin: 10 }}
+      />
+      <Button
+        disabled={progress === 0}
+        onPress={() => {
+          setProgress(0);
+        }}
+        title={'Restart'}
+        containerStyle={{ margin: 10 }}
+      />
+    </View>
+  </View>
+);
+};
 
-export default App;
+export default LinearProgressAPI;
