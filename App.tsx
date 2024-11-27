0e0330863f871cba6221ca7da4c528c2f9117557
Nhan Tri Danh <57444770+dnhan1707@users.noreply.github.com>
2024-11-27 13:23:13 -0800

    added comment for what was found

--- App.tsx
+++ App.tsx
@@ -5,6 +5,11 @@ import { Button, LinearProgress } from '@rneui/themed';
 const LinearProgressAPI: React.FunctionComponent = () => {
 const [progress, setProgress] = React.useState(0);
 
+
+// The issue is from LinearProgress, but it is the dependency we downloaded. 
+// May want to refer to ProgressBarAndroid from https://github.com/react-native-progress-view/progress-bar-android
+// to see how the progress prop accept float values.
+
 React.useEffect(() => {
   let subs = true;
   if (progress < 1 && progress !== 0) {
@@ -40,7 +45,7 @@ return (
       <Button
         disabled={progress > 0}
         onPress={() => {
-          setProgress(0.00001);
+          setProgress(0.0001);
         }}
         title={'Start Progress'}
         containerStyle={{ margin: 10 }}
